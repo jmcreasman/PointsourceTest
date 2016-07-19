17d60141b981ac1f99e12b689cd04dea86768c1a
Jon <jmcreasman89@gmail.com>
2016-07-19 12:18:41 -0400

    adding in gender specfic class plus call back error in angular controller

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,28 @@
-(function(){
+// (function(){
+//   var app = angular.module('pointsource', []);
+//   app.controller('UserProfile', ['$http', function($http){
+//     var profile = this;
+//
+//     profile.bio = [ ];
+//
+//     $http.get('http://applicant.pointsource.us/api/testUser/57869704f62a2d8f3c05da99').success(function(data){
+//       profile.bio = data;
+//     });
+//   }]);
+// })();
+(function() {
   var app = angular.module('pointsource', []);
-  app.controller('UserProfile', ['$http', function($http){
+  app.controller('UserProfile', ['$http', function($http) {
     var profile = this;
 
-    profile.bio = [ ];
+    profile.bio = [];
 
-    $http.get('http://applicant.pointsource.us/api/testUser/57869704f62a2d8f3c05da99').success(function(data){
-      profile.bio = data;
-    });
+    $http.get('http://applicant.pointsource.us/api/testUser/57869704f62a2d8f3c05da99')
+      .success(function(data) {
+        profile.bio = data;
+      })
+      .error(function(data, status) {
+        console.error('Repos error', status, data);
+      });
   }]);
 })();
